d67f0cf664e054b30e91bf6c253776832c3b2e70
lou <lou1997860213@gmail.com>
2022-01-25 02:14:16 +0800

    security

--- webServer_learning/mongodb_pratice.py
+++ webServer_learning/mongodb_pratice.py
@@ -2,9 +2,10 @@
 import pymongo
 
 # 連線到 MongoDB 雲端資料庫
-client=pymongo.MongoClient("mongodb+srv://lou8168168:lou10130244@mrdotcluster.r178b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db=client.MEMBER # 選擇要操作 test 資料庫
-collection=db.account # 選擇操作 users 集合
+client = pymongo.MongoClient(
+    "mongodb+srv://account:password@mrdotcluster.r178b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.MEMBER  # 選擇要操作 test 資料庫
+collection = db.account  # 選擇操作 users 集合
 
 # 新增資料
 # 集合.insert_one({data}) 插一筆
@@ -18,13 +19,12 @@ collection=db.account # 選擇操作 users 集合
 # })
 
 
-
 # 查詢資料
 # 集合.find/find_one(ObjectId(objID)/{查詢條件
 #   條件都必須成立用"$and":[{條件1},{條件2}...]
 #   條件擇一成立用"$or":[{條件1},{條件2}...]
 #   },sort=[(排序方式
-#       "排序參數", 
+#       "排序參數",
 #           升序用pymongo.ASCENDING
 #           降序用pymongo.DESCENDING
 #   ]) 回傳 doc
@@ -54,9 +54,8 @@ collection=db.account # 選擇操作 users 集合
 # print("此資料密碼",data["password"])
 
 
-
 # 更新資料
-# 集合.update_one/update_many({篩選條件},{更新資訊}) 
+# 集合.update_one/update_many({篩選條件},{更新資訊})
 #   前面的dictionary為條件,
 #   後面的dictionary為
 #       更新內容用"$set":{}
@@ -76,7 +75,7 @@ collection=db.account # 選擇操作 users 集合
 #     #     "password":"boni0312",
 #     #     "age":8 # 本來沒此欄位,會新增此欄位
 #     # }
-#     # "$unset":{ 
+#     # "$unset":{
 #     #     "age":100 # 撤掉 age 欄位
 #     # }
 #     # "$inc":{
@@ -90,7 +89,6 @@ collection=db.account # 選擇操作 users 集合
 # print("實際更新資料數量",result.modified_count)
 
 
-
 # 刪除資料
 # 集合.delete_one/delete_many({刪除條件})
 
@@ -100,12 +98,12 @@ collection=db.account # 選擇操作 users 集合
 # })
 # print("符合刪除條件數量",result.delete_count)
 
-collect=collection.find()
+collect = collection.find()
 print("當前資料")
 for entity in collect:
     print(entity)
 
-collect=collection.find()
+collect = collection.find()
 print("更新後資料")
 for entity in collect:
     print(entity)
